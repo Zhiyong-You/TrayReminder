2c6bcd27972013bdcdc425545652d751bb39beb4
zhiyong.you <zhiyong.you@aimnext.com>
2026-04-22 21:46:53 +0900

    ico設定

--- Services/TrayIconService.cs
+++ Services/TrayIconService.cs
@@ -1,4 +1,5 @@
 using System.Drawing;
+using System.Reflection;
 using System.Windows.Forms;
 using TrayReminder.Views;
 
@@ -10,6 +11,7 @@ public class TrayIconService : IDisposable
     private readonly MainWindow _mainWindow;
     private readonly Action _openNewReminder;
     private readonly Action _exitApp;
+    private readonly Icon? _customIcon;
     private bool _disposed;
 
     public TrayIconService(MainWindow mainWindow, Action openNewReminder, Action exitApp)
@@ -18,15 +20,33 @@ public class TrayIconService : IDisposable
         _openNewReminder = openNewReminder;
         _exitApp = exitApp;
 
+        _customIcon = LoadAppIcon();
+
         _notifyIcon = new NotifyIcon
         {
-            Icon = SystemIcons.Application,
+            Icon = _customIcon ?? SystemIcons.Application,
             Text = "TrayReminder",
             Visible = true,
             ContextMenuStrip = BuildContextMenu()
         };
 
         _notifyIcon.DoubleClick += (_, _) => ShowMainWindow();
+    }
+
+    // TrayReminder.ico をアセンブリの EmbeddedResource から読み込む
+    // 失敗時は null を返し、呼び出し側で SystemIcons.Application にフォールバックする
+    private static Icon? LoadAppIcon()
+    {
+        try
+        {
+            var stream = Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream("TrayReminder.TrayReminder.ico");
+            return stream is not null ? new Icon(stream) : null;
+        }
+        catch
+        {
+            return null;
+        }
     }
 
     private ContextMenuStrip BuildContextMenu()
@@ -53,5 +73,6 @@ public class TrayIconService : IDisposable
         _disposed = true;
         _notifyIcon.Visible = false;
         _notifyIcon.Dispose();
+        _customIcon?.Dispose();
     }
 }
